c3f6d450eff58f1b3c75f302adaf34638e0a8f96
Abhay Amin <pixoloproductions@gmail.com>
2015-07-29 01:11:43 +0530

    first look

---
+++ juggle buggle/Assets/reset.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+		// Get movement of the finger since last frame
+		transform.position = Vector3(0f, 7.20f, 0f);
+	}
+}
