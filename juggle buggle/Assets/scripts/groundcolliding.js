0b54490262cb0124521e2229e1024f9a5dacdc46
abhay.pixolo <abhay.pixolo@gmail.com>
2015-07-31 21:53:10 +0530

    button images

---
+++ juggle buggle/Assets/scripts/groundcolliding.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+public var ballcollider : Collider2D;
+
+public var groundcollider:Collider2D;
+function Start () {
+
+}
+
+function Update () {
+if(groundcollider.IsTouching(ballcollider))
+{
+ Application.LoadLevel ("menu");
+Debug.Log('count');
+}
+
+}
--- juggle buggle/Assets/scripts/playeraccelrometer.js
+++ juggle buggle/Assets/scripts/playeraccelrometer.js
@@ -1,9 +1,45 @@
 ﻿#pragma strict
+var touch:boolean;
+var position:Vector3;
+public var ballcollider : Collider2D;
+public var playercollider : Collider2D;
 
 function Start () {
+touch=true;
 
 }
 
 function Update () {
-transform.Translate(Input.acceleration.x, 0, 0);
+
+if(touch==true)
+{
+if(playercollider.IsTouching(ballcollider))
+{
+touch=false;
+Debug.Log('count');
+}
+}
+if(touch==false)
+{
+if(!playercollider.IsTouching(ballcollider))
+{
+touch=true;
+Debug.Log('count1');
+}
+}
+
+
+ if (Input.GetKeyDown(KeyCode.LeftArrow))
+                 {
+                         position = this.transform.position;
+                         position.x = position.x - 0.3f;
+                         this.transform.position = position;
+                 }
+                 if (Input.GetKeyDown(KeyCode.RightArrow))
+                 {
+                         position = this.transform.position;
+                         position.x = position.x+ 0.3f;
+                         this.transform.position = position;
+                 };
+//transform.Translate(Input.acceleration.x, 0, 0);
 }
